a8391601cc8eaf6079fa3df5119fed1f88994023
Barkley <barkley.santo@gmail.com>
2023-02-16 10:44:01 +0800

    2 grids in App.js , left and right side. Left for List right for Map - map side is larger

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react"
+import Header from "./components/Header/Header"
+import List from "./components/List/List"
+import Map from "./components/Map/Map"
+// import PlaceDetails from "./components/PlaceDetails/PlaceDetails"
+import { CssBaseline, Grid } from "@material-ui/core"
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          {/*This grid takes full width on mobile, md&lg only 4 spaces (out of 12) */}
+          <List />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Map />
+        </Grid>
+      </Grid>
+
+      {/* <PlaceDetails /> */}
+    </>
   )
 }
 
